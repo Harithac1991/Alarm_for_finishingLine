d966d63588c8e24590d82433196d937b6fe3309a
haritha.c <haritha.c@xe04.ey.com>
2018-12-30 18:07:18 +0530

    2 python files and one sound file

---
+++ BeepSound_finish.py
@@ -0,0 +1,4 @@
+import winsound
+duration = 1000  # millisecond
+freq = 440  # Hz
+winsound.Beep(freq, duration)
---
+++ SmashSound_Finish.py
@@ -0,0 +1,4 @@
+def win_wav_play(filename):
+    import winsound
+    winsound.PlaySound(filename, winsound.SND_FILENAME) 
+win_wav_play("Smashing-Yuri_Santana-1233262689.wav")
